803cf2dab0d4ed854c6885f893fbd8c3a9668aba
pasupathi1997 <pasupathi1261997@gmail.com>
2023-02-09 10:16:56 +0530

    done

---
+++ src/org/demo/Start.java
@@ -0,0 +1,15 @@
+package org.demo;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Start {
+
+ public static void main(String[] args) {
+	 Map<Integer, String> m = new Hashtable<>();
+	 m.put(12, "rickj");
+	 System.out.println(m);
+	
+}
+
+}
